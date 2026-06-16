7c83d8a457cc1320fd5f7200282d872f9ae894e1
Oleksandr Aleksieiev <oleksandr.aleksieiev@teleport.de>
2026-06-16 11:11:35 +0200

    fix init script

--- init/02_users.sql
+++ init/02_users.sql
@@ -1,18 +1,24 @@
 USE ticket_system;
 
-CREATE IF NOT EXISTS USER 'admin'@'%' IDENTIFIED BY 'admin';
-CREATE IF NOT EXISTS USER 'admin'@'localhost' IDENTIFIED BY 'admin';
-CREATE IF NOT EXISTS USER 'app_user'@'%' IDENTIFIED BY 'app_user';
-CREATE IF NOT EXISTS USER 'app_user'@'localhost' IDENTIFIED BY 'app_user';
-CREATE IF NOT EXISTS USER 'report_user'@'%' IDENTIFIED BY 'report_user';
-CREATE IF NOT EXISTS USER 'report_user'@'localhost' IDENTIFIED BY 'report_user';
+-- Create users
+CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'admin';
+CREATE USER IF NOT EXISTS 'admin'@'localhost' IDENTIFIED BY 'admin';
 
+CREATE USER IF NOT EXISTS 'app_user'@'%' IDENTIFIED BY 'app_user';
+CREATE USER IF NOT EXISTS 'app_user'@'localhost' IDENTIFIED BY 'app_user';
+
+CREATE USER IF NOT EXISTS 'report_user'@'%' IDENTIFIED BY 'report_user';
+CREATE USER IF NOT EXISTS 'report_user'@'localhost' IDENTIFIED BY 'report_user';
+
+-- Grant privileges to users
 GRANT ALL PRIVILEGES ON ticket_system.* TO 'admin'@'%' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON ticket_system.* TO 'admin'@'localhost' WITH GRANT OPTION;
-GRAND CREATE ON *.* TO 'admin'@'%';
-GRAND CREATE ON *.* TO 'admin'@'localhost';
+GRANT CREATE ON *.* TO 'admin'@'%';
+GRANT CREATE ON *.* TO 'admin'@'localhost';
+
 GRANT SELECT, INSERT, UPDATE, DELETE ON ticket_system.* TO 'app_user'@'%';
 GRANT SELECT, INSERT, UPDATE, DELETE ON ticket_system.* TO 'app_user'@'localhost';
+
 GRANT SELECT ON ticket_system.* TO 'report_user'@'%';
 GRANT SELECT ON ticket_system.* TO 'report_user'@'localhost';
 
